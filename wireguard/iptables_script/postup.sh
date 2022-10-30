e2fbc4247595ccae1d9e2c794be42a4f6b0b52e3
yuruto-free <contact-form@yururi-do.com>
2022-10-30 19:50:15 +0900

    update wireguard iptables

---
+++ wireguard/iptables_script/postup.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+readonly base_dir=$(cd $(dirname $0) && pwd)
+
+ls ${base_dir}/conf.up.d/*.conf | while read config_file; do
+    echo "[#] - ${config_file}"
+    cat ${config_file} | while read cmd; do
+        echo "[#]   ${cmd}"
+        eval "${cmd}"
+    done
+done
